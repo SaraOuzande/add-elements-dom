9a595f0a0ccea4c55a23bdcd56beb71a6cef0ef2
Sara Ouzande <ouzandeitu@proton,me>
2024-11-15 22:06:03 +0100

    JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
  // Aquí tu código
+
+const botonAgregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+
+botonAgregar.addEventListener('click', () => {
+
+    const nuevoElemento = prompt('Introduce un elemento para agregar a la lista:');
+
+   
+    if (nuevoElemento) {
+        
+        const li = document.createElement('li');
+        li.textContent = nuevoElemento;
+
+       
+        lista.appendChild(li);
+    } else {
+        alert('No ingresaste ningún elemento.');
+    }
+});
+
+ 
